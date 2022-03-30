b64125fae99982b9208ddc49a235da2bbf9bcc22
Ian L <ileach81@gmail.com>
2022-03-29 20:55:15 -0400

    added typedefs gql

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,2 +1,30 @@
 const { gql } = require("apollo-server-express");
-const typeDefs = gql``;
+const typeDefs = gql`
+  type User {
+    _id: ID
+    username: String
+    email: String
+    comments: [Comment]
+  }
+  type Auth {
+    token: ID!
+    user: User
+  }
+  type Comment {
+    _id: ID
+    thoughtText: String
+    createdAt: String
+    username: String
+    reactionCount: Int
+    reactions: [Reaction]
+  }
+
+  type Reaction {
+    _id: ID
+    reactionBody: String
+    createdAt: String
+    username: String
+  }
+`;
+// export the typeDefs
+module.exports = typeDefs;
