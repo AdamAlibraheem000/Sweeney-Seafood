a2b1f46a36df20365c0d7b98bf43b07940e1c981
Shinichi <miyakawashi@gmail.com>
2022-03-31 19:01:08 -0400

    front end gql add user, add comment, add reply mutations

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -11,3 +11,37 @@ export const LOGIN = gql`
         }
     }
 `;
+
+export const ADD_USER = gql`
+    mutation addUser($username: String!, $email: String!, $password: String!) {
+        addUser(username: $username, email: $email, password: $password) {
+            token
+            user {
+                _id
+                username
+            } 
+        }
+    }
+`;
+
+export const ADD_COMMENT = gql`
+    mutation addComment($thoughtText: String!) {
+        addComment(thoughtText: $thoughtText) {
+            _id
+            thoughtText
+            createdAt
+            username
+        }
+    }
+`;
+
+export const ADD_REPLY = gql`
+    mutation addReply($commentId: ID!, $replyBody: String!) {
+        addReply(commentId: $commentId, replyBody: $replyBody) {
+            _id
+            replyBody
+            createdAt
+            username
+        }
+    }
+`;
