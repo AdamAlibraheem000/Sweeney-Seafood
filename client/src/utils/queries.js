69dcad11292b13a7384727958013deddde9d7311
Shinichi <miyakawashi@gmail.com>
2022-03-31 18:52:01 -0400

    front end gql query users/user

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -5,7 +5,6 @@ export const QUERY_ME = gql`
     me {
         _id
         username
-        email
         comments {
             _id
             thoughtText
@@ -47,6 +46,33 @@ export const QUERY_COMMENT = gql`
                 replyBody
                 createdAt
                 username
+            }
+        }
+    }
+`;
+
+export const QUERY_USERS = gql`
+    {
+        users {
+            _id
+            username
+        }
+    }
+`;
+
+export const QUERY_USER = gql`
+    query user($username: String!) {
+        user(username: $username) {
+            _id
+            username
+            comments {
+                _id
+                thougthText
+                createdAt
+                replies {
+                    _id
+                    replyBody
+                }
             }
         }
     }
