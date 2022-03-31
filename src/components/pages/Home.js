a98f3338a103ce5ead1582965de440b8e000e6b8
AdamAlibraheem <shrineknight000@gmail.com>
2022-03-31 11:56:58 -0400

    updated header & footer styling

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -17,10 +17,10 @@ export default function Home() {
     <FadeIn>
     <div className="title-flex">
       <h1 className="title-font">
-        Sweeney's<span className="copyright">©</span> Seafood
+        Rated Best Seafood
         
       </h1>
-      <h1 className="sub-title">Bar and Grill</h1>
+      <h1 className="sub-title">In Dayton</h1>
       
     </div>
     </FadeIn>
