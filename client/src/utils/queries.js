edfec16a052bd416d24581c0e9ad9c2fd88f6d93
Shinichi <miyakawashi@gmail.com>
2022-03-31 18:48:30 -0400

    front end gql query comments/comment

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -19,3 +19,35 @@ export const QUERY_ME = gql`
         }
     }
 }`;
+
+export const QUERY_COMMENTS = gql`
+    query comments($username: String) {
+        comments(username: $username) {
+            _id
+            thoughText
+            createdAt
+            username
+            replies {
+                replyBody
+                createdAt
+                username
+            }
+        }
+    }
+`;
+
+export const QUERY_COMMENT = gql`
+    query comment($id: ID!) {
+        comment(id: $id) {
+            _id
+            thoughtText
+            createdAt
+            username
+            replies {
+                replyBody
+                createdAt
+                username
+            }
+        }
+    }
+`;
