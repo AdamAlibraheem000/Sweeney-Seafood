373dc2dd62369afce412e85fcaacce35a87e3f45
Shinichi <miyakawashi@gmail.com>
2022-03-31 18:43:24 -0400

    front end gql query me()

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,3 +1,21 @@
 import { gql } from '@apollo/client';
 
-export const QUERY_ME = gql``;
+export const QUERY_ME = gql`
+{
+    me {
+        _id
+        username
+        email
+        comments {
+            _id
+            thoughtText
+            username
+            replyCount
+            replies {
+                _id
+                replyBody
+                username
+            }
+        }
+    }
+}`;
