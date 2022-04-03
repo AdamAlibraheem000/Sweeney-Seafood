88c08154bfc77c810c2db34d304b49a8219cc97c
Ian L <ileach81@gmail.com>
2022-04-03 10:51:32 -0400

    server

--- server/server.js
+++ server/server.js
@@ -17,3 +17,9 @@ const server = new ApolloServer({
 server.applyMiddleware({ app });
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+db.once("open", () => {
+  app.listen(PORT, () => {
+    console.log(`API server running on port ${PORT}!`);
+  });
+});
