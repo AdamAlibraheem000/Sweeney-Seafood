7df1277b4ede87444f2c26c4e865ef070b98a68e
AdamAlibraheem <shrineknight000@gmail.com>
2022-04-04 19:11:46 -0400

    modified navbar

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -17,35 +17,35 @@ export default function Home() {
       </title>
     </Helmet>
     
-    {/* <div className="back-img"> */}
-    {/* <FadeIn> */}
-    {/* <div className="title-flex">
+    {/* <div className="back-img"> 
+    <FadeIn>
+    <div className="title-flex">
       <h1 className="title-font">
         Rated Best Seafood
         
       </h1>
       <h1 className="sub-title">In Dayton</h1>
       
-    </div> */}
-    {/* </FadeIn> */}
-    {/* </div> */}
+    </div>
+    </FadeIn>
+     </div> */}
 
-    <div>Testing
+    {/* <div>Testing
     </div>
     <div>Testing
     </div>
     <div>Testing
-    </div>
+    </div> */}
 {/* <div id="myCarousel" class="carousel slide" data-ride="carousel">
-  <!-- Indicators -->
-  <ol class="carousel-indicators">
+  {/* <!-- Indicators --> */}
+  {/* <ol class="carousel-indicators">
     <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
     <li data-target="#myCarousel" data-slide-to="1"></li>
     <li data-target="#myCarousel" data-slide-to="2"></li>
-  </ol>
+  </ol> */} 
 
-  <!-- Wrapper for slides -->
-  <div class="carousel-inner">
+  {/* <!-- Wrapper for slides --> */}
+  {/* <div class="carousel-inner">
     <div class="item active">
       <img src={fishPic} alt="Los Angeles" />
     </div>
@@ -57,10 +57,10 @@ export default function Home() {
     <div class="item">
       <img src={whiskey} alt="New York" />
     </div>
-  </div>
+  </div> */}
 
-  <!-- Left and right controls -->
-  <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+  {/* <!-- Left and right controls --> */}
+  {/* <a class="left carousel-control" href="#myCarousel" data-slide="prev">
     <span class="glyphicon glyphicon-chevron-left"></span>
     <span class="sr-only">Previous</span>
   </a>
@@ -68,12 +68,12 @@ export default function Home() {
     <span class="glyphicon glyphicon-chevron-right"></span>
     <span class="sr-only">Next</span>
   </a>
-</div> */}
+</div>  */}
 
-<Carousel>
+ <Carousel>
   <Carousel.Item>
     <img
-      className="d-block w-100"
+      className="d-block w-100 back-img"
       src="holder.js/800x400?text=First slide&bg=373940"
       alt="First slide"
     />
@@ -118,8 +118,8 @@ export default function Home() {
       <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
     </Carousel.Caption>
   </Carousel.Item>
-</Carousel>
+</Carousel> 
 
-    </>
+    </> 
   );
 }
