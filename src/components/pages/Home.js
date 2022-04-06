ffb28cfb83350969dc4edf0171b0e40554001b17
AdamAlibraheem <shrineknight000@gmail.com>
2022-04-06 13:17:55 -0400

    update carousel

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -14,67 +14,14 @@ export default function Home() {
     <Helmet>
       <title>  
         Sweeney's Seafood Bar & Grill
-      
       </title>
     </Helmet>
-    
-    {/* <div className="back-img"> 
-    <FadeIn>
-    <div className="title-flex">
-      <h1 className="title-font">
-        Rated Best Seafood
-        
-      </h1>
-      <h1 className="sub-title">In Dayton</h1>
-      
-    </div>
-    </FadeIn>
-     </div> */}
 
-    {/* <div>Testing
-    </div>
-    <div>Testing
-    </div>
-    <div>Testing
-    </div> */}
-{/* <div id="myCarousel" class="carousel slide" data-ride="carousel">
-  {/* <!-- Indicators --> */}
-  {/* <ol class="carousel-indicators">
-    <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
-    <li data-target="#myCarousel" data-slide-to="1"></li>
-    <li data-target="#myCarousel" data-slide-to="2"></li>
-  </ol> */} 
-
-  {/* <!-- Wrapper for slides --> */}
-  {/* <div class="carousel-inner">
-    <div class="item active">
-      <img src={fishPic} alt="Los Angeles" />
-    </div>
-
-    <div class="item">
-      <img src={danLance} alt="Chicago" />
-    </div>
-
-    <div class="item">
-      <img src={whiskey} alt="New York" />
-    </div>
-  </div> */}
-
-  {/* <!-- Left and right controls --> */}
-  {/* <a class="left carousel-control" href="#myCarousel" data-slide="prev">
-    <span class="glyphicon glyphicon-chevron-left"></span>
-    <span class="sr-only">Previous</span>
-  </a>
-  <a class="right carousel-control" href="#myCarousel" data-slide="next">
-    <span class="glyphicon glyphicon-chevron-right"></span>
-    <span class="sr-only">Next</span>
-  </a>
-</div>  */}
-
- <Carousel className='main-container'>
+  <FadeIn>
+ <Carousel className='main-container carousel-fade' data-interval="5">
   <Carousel.Item> 
     <img
-      className="d-block w-100 back-img"
+      className="d-block  back-img"
       // src={rockfeller}
       alt="First slide"
     />
@@ -90,21 +37,22 @@ export default function Home() {
       alt="Second slide"
     />
 
-    <Carousel.Caption>
-      <h3>Second slide label</h3>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+<Carousel.Caption className= "carousel-flex">
+      <h1 className="title-font">Located In The Heart </h1>
+      <h1 className="sub-title">Of Centerville’s Historic District</h1>
     </Carousel.Caption>
   </Carousel.Item>
   <Carousel.Item>
     <img
-      className="d-block w-100 back-img-3"
+      className="d-block w-100 h-90 back-img-3"
       // src ={danLance}
       alt="Third slide"
     />
 
-    <Carousel.Caption className= "carousel-flex">
-      <h1 className="title-font">Come Visit Doug's Bar</h1>
-      <h1 className="sub-title">With a wide variety of Liquors and Spirits</h1>
+    <Carousel.Caption >
+      <div className= "carousel-flex">
+      <h1 className="dan-font">Come Visit Doug's Bar...</h1>
+      </div>
     </Carousel.Caption>
   </Carousel.Item>
   <Carousel.Item>
@@ -115,11 +63,13 @@ export default function Home() {
     />
 
     <Carousel.Caption>
-      <h3>Third slide label</h3>
-      <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+    <div className= "carousel-flex">
+      <h1 className="dansub-title">With a wide variety of Liquors, Wine, and Craft Beers.</h1>
+      </div>
     </Carousel.Caption>
   </Carousel.Item>
 </Carousel> 
+</FadeIn>
 
 
     </> 
