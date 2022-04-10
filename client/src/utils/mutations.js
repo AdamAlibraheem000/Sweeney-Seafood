f855ae19b9993d358675313c8d80747f192ebdc1
ingramwd <Ingramwd01@gmail.com>
2022-04-10 17:10:29 -0400

    added commentForm

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,16 @@
+import { gql } from '@apollo/client';
+
+export const ADD_COMMENT = gql`
+  mutation addComment($commentText: String!) {
+    addComment(commentText: $commentText) {
+      _id
+      commentText
+      createdAt
+      username
+      replyCount
+      replies {
+        _id
+      }
+    }
+  }
+`;
