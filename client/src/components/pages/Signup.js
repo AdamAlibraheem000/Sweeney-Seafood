9bae5936839e0c05e093034186791f097d22d9a3
Ian L <ileach81@gmail.com>
2022-04-12 18:28:41 -0400

    form submit

--- client/src/components/pages/Signup.js
+++ client/src/components/pages/Signup.js
@@ -11,4 +11,29 @@ const Signup = () => {
     password: "",
   });
   const [addUser, { error }] = useMutation(ADD_USER);
+
+  // state update
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+
+    setFormState({
+      ...formState,
+      [name]: value,
+    });
+  };
+
+  // submit form
+  const handleFormSubmit = async (event) => {
+    event.preventDefault();
+
+    try {
+      const { data } = await addUser({
+        variables: { ...formState },
+      });
+
+      Auth.login(data.addUser.token);
+    } catch (e) {
+      console.error(e);
+    }
+  };
 };
