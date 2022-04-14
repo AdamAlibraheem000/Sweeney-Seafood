81716ccdb1f3ca8857e5a2c72fe1eb41863c8cf0
AdamAlibraheem <shrineknight000@gmail.com>
2022-04-13 21:16:15 -0400

    try

--- client/src/components/PortfolioContainer.js
+++ client/src/components/PortfolioContainer.js
@@ -46,7 +46,7 @@ export default function PortfolioContainer() {
   return (
     <div className="main">
       {/* We are passing the currentPage from state and the function to update it */}
-      <NavTabs currentPage={currentPage} handlePageChange={handlePageChange} />
+      {/* <NavTabs currentPage={currentPage} handlePageChange={handlePageChange} /> */}
       {/* Here we are calling the renderPage method which will return a component  */}
       {renderPage()}
     </div>
