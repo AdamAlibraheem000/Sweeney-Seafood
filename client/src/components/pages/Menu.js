0b16a82b5028ca547dce3c4c02f1d20b03d33d44
AdamAlibraheem <shrineknight000@gmail.com>
2022-05-05 13:55:27 -0400

    modified menu img

--- client/src/components/pages/Menu.js
+++ client/src/components/pages/Menu.js
@@ -9,7 +9,10 @@ export default function Contact() {
       <title>Sweeney’s - Dinner Menu</title>
     </Helmet>
     <FadeIn>
-    <div className="bgm-img-menu"></div>
+      <div>
+        <img className="bgm-img-menu"></img>
+      </div>
+    {/* <div className="bgm-img-menu"></div> */}
     </FadeIn>
     <section className="menu-bgn-grid">
       <article className='display-mobile-none'>
