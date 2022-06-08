71ba6df0b5c64f952b9537cd1dc82dfefa6acd21
AdamAlibraheem <shrineknight000@gmail.com>
2022-06-08 12:13:39 -0400

    added basic routes & user model

---
+++ server/routes/api/auth.js
@@ -0,0 +1,9 @@
+// JSON webtoken for authentication
+const express = require("express");
+const router = express.Router();
+
+
+
+router.get('/', (req, res) => res.send('Auth route'))
+
+module.exports = router;
--- server/server.js
+++ server/server.js
@@ -1,8 +1,17 @@
 const express = require('express');
+const connectDB = require('./config/db');
 
 const app = express();
 
+// Connect database
+connectDB();
+
 app.get('/', (req, res) => res.send('API running'));
+
+// Define routes
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/features', require('./routes/api/features'));
 
 const PORT = process.env.PORT || 3001;
 
