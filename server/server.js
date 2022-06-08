e864027331e992a0f77001b4ce99897a70c980d3
AdamAlibraheem <shrineknight000@gmail.com>
2022-06-08 11:17:20 -0400

    removed backend. Added basic express server

--- server/server.js
+++ server/server.js
@@ -1,43 +1,10 @@
-const express = require("express");
-const { ApolloServer } = require("apollo-server-express");
-const path = require("path");
+const express = require('express');
 
-const { typeDefs, resolvers } = require("./schemas");
-const { authMiddleware } = require("./utils/auth");
-const db = require("./config/connection");
-
-const PORT = process.env.PORT || 3001;
 const app = express();
 
-const startServer = async () => {
-  const server = new ApolloServer({
-    typeDefs,
-    resolvers,
-    context: authMiddleware,
-  });
+app.get('/', (req, res) => res.send('API running'));
 
-  await server.start();
-  server.applyMiddleware({ app });
-  console.log(`Use GraphQL at http://localhost:${PORT}`);
-};
-
-startServer();
+const PORT = process.env.PORT || 3001;
 
 
-
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
-
-
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
-}
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/build/index.html'));
-});
-
-db.once("open", () => {
-  app.listen(PORT, () => {
-    console.log(`API server running on port ${PORT}!`);
-  });
-});
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
