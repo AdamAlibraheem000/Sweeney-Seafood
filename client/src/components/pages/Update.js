4af41d57aa813bcce2c57f3e1d812912cce1d543
AdamAlibraheem000 <adamalibraheem@hotmail.com>
2022-06-28 13:57:58 -0400

    added styling for update component

--- client/src/components/pages/Update.js
+++ client/src/components/pages/Update.js
@@ -50,17 +50,7 @@ function Update() {
       <section className='update-form-styling'>
         <h1>Add New Feature</h1>
     <form  onSubmit={changeOnClick} encType='multipart/form-data'>
-  <div className="form-group">
-    <label htmlFor="authorname">Author Name</label>
-    <input 
-    type="text"
-    value={authorname} 
-    className="form-control"  
-    placeholder="Auther Name"
-    onChange={e => setAuthorName(e.target.value)}
-    />
-  </div>
-  <div className="form-group">
+    <div className="form-group">
     <label htmlFor="title">Title</label>
     <input 
     type="text" 
@@ -71,14 +61,27 @@ function Update() {
     />
   </div>
   <div className="form-group">
-    <label htmlFor='article'>Article</label>
+    <label htmlFor='article'>Desc</label>
     <textarea 
     className="form-control" 
     rows="3"
     value={article}
+    placeholder="Enter Description"
     onChange={e => setArticle(e.target.value)}
     ></textarea>
   </div>
+  <div className="form-group">
+    <label htmlFor="authorname">Price</label>
+    <input 
+    type="text"
+    value={authorname} 
+    className="form-control"  
+    placeholder="Enter Price"
+    onChange={e => setAuthorName(e.target.value)}
+    />
+  </div>
+  
+  
   <button type="submit" className="btn btn-primary">Post Article</button>
 </form>
 </section>
